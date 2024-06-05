1c990f304fdfdd5861346df5995ca146f867c67b
jiml268 <114753310+jiml268@users.noreply.github.com>
2024-06-05 08:53:35 -0400

    set up server.js and folder structure for backend

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const session = require('express-session');
+const sess = {
+  secret: process.env.JWT_privateKey,
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+};
+app.use(session(sess));
+
+app.use(cors());
+app.use(express.json());
+app.all('/api', function(req, res) {
+    return res.status(200).json({
+        code: 200,
+        Massage: "Corrected",
+      });
+})
+
+// app.use("/api", usersRoutes);
+// app.use("/api", addressRoutes);
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
