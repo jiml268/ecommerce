b39b842d3cb78b709e96f3b2845dc160ba07da13
jiml268 <114753310+jiml268@users.noreply.github.com>
2024-06-07 13:55:26 -0400

    setup list all addresses

--- client/src/pages/AllAddresses/AllAddresses.jsx
+++ client/src/pages/AllAddresses/AllAddresses.jsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 import { getAllAddress } from "../../redux/addresses/addressOperators"
 import { getID } from "../../redux/users/usersSelectors"
 import { useLocation } from "react-router-dom";
+import styles from "./AllAddresses.module.css"
 
 function AllAddresses() {
  const dispatch = useDispatch()
@@ -35,20 +36,23 @@ function AllAddresses() {
 
    
     return (
-        <>
+        <div>
+            <h1> Your {data.type} Addresses</h1>
+        <div className={styles.listAddresses}>
             {showAddresses.length > 0 &&
                 showAddresses.map(function(address) {
       return (
           <div key={address.id}>
-              <div style={{border: "1px solid black"}}>
+              <div className ={ styles.AddressBlock} style={{border: "1px solid black"}}>
               {address.default_addtress === 1 &&
-                  <h3>Default</h3>
+                  <h3 className={styles.default}>Default</h3> 
               }
-              <h3>{address.first_name} {address.middle_init} {address.last_name} </h3>
+              <h3 style={{fontWeight: 800}}>{address.first_name} {address.middle_init} {address.last_name} </h3>
               <h3>{address.address_line1}</h3>
               <h3>{address.address_line2}</h3>
             <h3>{address.city},{address.state} {address.zip} </h3>
-                  <h3>{address.Phone_num}</h3>
+                  <h3>Phone number: </h3>
+                   <h3>{address.Phone_num}</h3>
                   <button>edit</button>      
                   <button>remove</button>
                   {address.default_addtress !== 1 &&  <button>set as default</button>}
@@ -59,7 +63,8 @@ function AllAddresses() {
     })
 
         }
-        </>
+            </div>
+            </div>
     )
 }
 
